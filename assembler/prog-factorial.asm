3549a712fce7c32d7541b2133a41115f29df0cd2
umarnurmatov <umarkhonnur12@gmail.com>
2025-10-16 16:36:44 +0300

    #ADDED factorial-counting program

--- assembler/prog-factorial.asm
+++ assembler/prog-factorial.asm
@@ -1,9 +1,26 @@
-CALL :1
-PUSH 2
+PUSH  3
+POPR  S0
+CALL  :1
+PUSHR S1
 OUT
 HLT
-:1
-PUSH 1
-OUT
-POPR RAX
+
+:1       # factorial
+
+PUSHR S0
+PUSH  1
+JE    :2 # n == 1: return 1
+PUSHR S0 # s0: n, s: ret, n
+PUSHR S0 # s0: n, s: ret, n, n
+PUSH  1
+SUB
+POPR  S0 # s0: n-1, s: ret, n
+CALL  :1 
+PUSHR S1 # s1: (n-1)!, s: ret, n, (n-1)!
+MUL
+POPR  S1 # s1: n!
+RET
+:2
+PUSH  1
+POPR  S1
 RET
