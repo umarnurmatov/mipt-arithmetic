dbb4575cece6dfbb6c2bc248fc7cd2324ac57d74
umarnurmatov <umarkhonnur12@gmail.com>
2025-10-17 19:53:58 +0300

    #ADDED T-emporary and A-rg registers

--- common/commands.h
+++ common/commands.h
@@ -125,5 +125,13 @@ const proc_reg_t proc_regs[] =
     { "S1", 0x01 }, // saved
     { "S2", 0x02 }, // saved
     { "S3", 0x03 }, // saved
+    { "T0", 0x04 }, // temporary
+    { "T1", 0x05 }, // temporary
+    { "T2", 0x06 }, // temporary
+    { "T3", 0x07 }, // temporary
+    { "A0", 0x08 }, // arg / ret
+    { "A1", 0x09 }, // arg / ret
+    { "A2", 0x0A }, // arg
+    { "A3", 0x0B }, // arg
 };
 
