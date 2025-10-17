082ccc0de5daa484164b9f9edcb2006a075d43fb
umarnurmatov <umarkhonnur12@gmail.com>
2025-10-17 20:02:09 +0300

    #MODIFIED factorial in accordance with new reg naming convention

--- assembler/prog-factorial.asm
+++ assembler/prog-factorial.asm
@@ -1,25 +1,25 @@
 PUSH  3
-POPR  S0
+POPR  A0
 CALL  :factorial
-PUSHR S1
+PUSHR A1
 OUT
 HLT
 
 :factorial
-PUSHR S0
+PUSHR A0
 PUSH  1
 JE    :n_is_one
-PUSHR S0 # s0: n, s: ret, n
-PUSHR S0 # s0: n, s: ret, n, n
+PUSHR A0 # a0: n, s: ret, n
+PUSHR A0 # a0: n, s: ret, n, n
 PUSH  1
 SUB
-POPR  S0 # s0: n-1, s: ret, n
+POPR  A0 # a0: n-1, s: ret, n
 CALL  :factorial 
-PUSHR S1 # s1: (n-1)!, s: ret, n, (n-1)!
+PUSHR A1 # a1: (n-1)!, s: ret, n, (n-1)!
 MUL
-POPR  S1 # s1: n!
+POPR  A1 # a1: n!
 RET
 :n_is_one
 PUSH  1
-POPR  S1
+POPR  A1
 RET
